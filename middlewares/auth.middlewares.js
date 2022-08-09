37347946de336c75def262ab63a90b2f7931cbbf
hugotata78 <hugotatarinoff@gmail.com>
2022-08-09 10:54:33 -0300

    Se creo middleware de autenticación

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,22 @@
+import  User from '../models/user'
+import jwt from 'jsonwebtoken'
+
+const Auth = async (req, res, next) => {
+    try {
+        const strToken = req.headers.authorization
+
+        if (!strToken) return res.status(403).json({msg:'Debes iniciar sesión!'})
+        const token = strToken.includes(' ') ? strToken.split(' ')[1] : strToken
+        const key = jwt.verify(token, 'secret')
+        const user = await User.findOne({ _id: key.id })
+        if(!user) return res.status(401).json('Acceso denegado!')
+        req.user = user
+        console.log(user)
+        next()
+
+    } catch (error) {
+        res.status(500).json({error})
+    }
+}
+
+export default Auth
