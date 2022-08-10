799d010034e9760926f26abe209cb71256b45cb3
hugotata78 <hugotatarinoff@gmail.com>
2022-08-10 11:10:47 -0300

    se creo middlewares isAdmin e isModerator, se agregó a rutas tech para probar funcionamiento

--- routes/technology.route.js
+++ routes/technology.route.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
 
 import technologyController from "../controllers/technology.controller";
-import Auth from "../middlewares/auth.middlewares";
+import { Auth, isAdmin, isModerator } from "../middlewares/auth.middlewares";
 
 const router = Router();
 const { add, list, query, update, remove } = technologyController;
 
-router.post("/tech", Auth, add);
+router.post("/tech", Auth, isModerator, add);
 
 router.get("/tech", list);
 
 router.get("/tech/:id", query);
 
-router.put("/tech/:id", update);
+router.put("/tech/:id", Auth, update);
 
-router.delete("/tech/:id", remove);
+router.delete("/tech/:id", Auth, remove);
 
 module.exports = router;
