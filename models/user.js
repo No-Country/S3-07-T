7a21feba065e4ab29b5bbf749ec84a1c97df34f5
unknown <cruzherreria1@gmail.com>
2022-08-14 12:17:07 -0300

    m

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs"
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-<<<<<<< HEAD
+
   firstName: { type: String, required: false },
   lastName: { type: String, required: false },
   email: { type: String, required: true },
@@ -19,21 +19,7 @@ const userSchema = new Schema({
   // idProject:{type:String,required:true},
   // role:Schema.Types.ObjectId,ref:"Role"
 });
-=======
-	firstName: { type: String, required: false },
-	lastName: { type: String, required: false },
-	email: { type: String, required: true, unique: true, lowercase: true },
-	password: { type: String, required: true },
-	phone:{type:String,required:true},
-	rolDes:{type:String,required:true},
-	roles: [{ type: Schema.Types.ObjectId, ref: "role" }],
-	avatar: { type: String, required: false },
-	description: { type: String, required: false }
-	// projects:{type:Schema.Types.ObjectId, ref:"Project"},
-	// idProject:{type:String,required:true},
-	// role:Schema.Types.ObjectId,ref:"Role"
-})
->>>>>>> 692840e6518aae3363df42aa2aa85c7db7401769
+
 
 userSchema.statics.passwordCode = async password => {
 	const encryp = await bcrypt.genSalt(10)
